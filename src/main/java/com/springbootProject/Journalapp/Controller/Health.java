c200a1d0eb302fdea64cf0bf06d2578d1fc1e5fd
Krishlay2107 <krishlaysharma3720@gmail.com>
2025-01-14 20:47:04 +0530

    "  it is used to check weather my controller is working fine or not "

---
+++ src/main/java/com/springbootProject/Journalapp/Controller/Health.java
@@ -0,0 +1,15 @@
+package com.springbootProject.Journalapp.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/health")
+public class Health {
+     public  String m1(){
+          return " i am fine ";
+     }
+
+}
+
+
