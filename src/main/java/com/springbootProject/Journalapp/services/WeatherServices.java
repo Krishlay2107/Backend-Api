8d087087b3030bd4375ae3f08bf0c85a513f67fb
Krishlay2107 <krishlaysharma3720@gmail.com>
2025-01-24 23:55:02 +0530

    How to usee external api in springboot

--- src/main/java/com/springbootProject/Journalapp/services/WeatherServices.java
+++ src/main/java/com/springbootProject/Journalapp/services/WeatherServices.java
@@ -4,18 +4,18 @@ import com.springbootProject.Journalapp.apiresponses.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherServices {
 
    @Autowired
      private  RestTemplate restTemplate;
 
-    public static final String apikey="8531beda04141a15168f6421e69343a1";
+    private    static final String apikey="8531beda04141a15168f6421e69343a1";
 
-    public static final String API = "http://api.weatherstack.com/current?access_key=ApiKey&query=CITY";
+    private static final String API = "http://api.weatherstack.com/current?access_key=ApiKey&query=CITY";
 
 
       public WeatherResponse  getWeather(String city){
