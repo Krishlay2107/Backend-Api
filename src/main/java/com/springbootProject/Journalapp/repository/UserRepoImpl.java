1acac4a5776f5ec0ccf4651c16e74f949ce77ea6
Krishlay2107 <krishlaysharma3720@gmail.com>
2025-01-26 01:27:49 +0530

    how ti create criteria Api  and its working

--- src/main/java/com/springbootProject/Journalapp/repository/UserRepoImpl.java
+++ src/main/java/com/springbootProject/Journalapp/repository/UserRepoImpl.java
@@ -18,8 +18,23 @@ public class UserRepoImpl {
     public List<User> getUserforSA(){
 
         Query query= new Query();
-        query.addCriteria(Criteria.where("userName").is("asu"));
+        // showing that find tye user of userName equals to asu ( way without creating the criteria object
+       // query.addCriteria(Criteria.where("userName").is("asu"));
+        //query.addCriteria(Criteria.where("age").is("21"));
+        // ypu can also write abpve using andoperator
+
+         //# by creating criterai object
+
+        // # match through the regex
+
+        Criteria criteria= new Criteria();
+         query.addCriteria(criteria.andOperator(
+                 Criteria.where("email").exists(true)
+        ,Criteria.where("sentimenatAnalysis").is(true)));
+
+      //  return  mongoTemplate.find(query,User.class); also return using this
          List<User> users=mongoTemplate.find(query,User.class);
+
           return users;
 
 
